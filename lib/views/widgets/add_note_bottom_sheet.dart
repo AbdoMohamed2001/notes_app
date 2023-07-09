751f33ca48b0a98a3138b90e251e70d0bf956970
AbdoMohamed2001 <abdo.mohammed1778@gmail.com>
2023-07-09 11:33:11 +0300

    validate user input

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -2,37 +2,68 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constants.dart';
 import 'package:notes_app/views/widgets/custom_text_field.dart';
 
-class AddNoteBottomSheet extends StatelessWidget {
+class AddNoteBottomSheet extends StatefulWidget {
   const AddNoteBottomSheet({Key? key}) : super(key: key);
 
   @override
+  State<AddNoteBottomSheet> createState() => _AddNoteBottomSheetState();
+}
+
+class _AddNoteBottomSheetState extends State<AddNoteBottomSheet> {
+
+  GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autoValidateMode = AutovalidateMode.disabled;
+  String? title,subTitle;
+  @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 22.0),
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 32,
-            ),
-            CustomTextField(
-              hintText: 'Title',
-            ),
-            SizedBox(
-              height: 18,
-            ),
-            CustomTextField(
-              hintText: 'Content',
-              maxLines: 5,
-            ),
-            SizedBox(
-              height: 62,
-            ),
-            CustomButton(),
-            SizedBox(
-              height: 18,
-            ),
-          ],
+    return  Form(
+      key: formKey,
+      autovalidateMode: autoValidateMode,
+      child:  Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 22.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 32,
+              ),
+              CustomTextField(
+                onSaved: (value){
+                  title = value;
+                },
+                hintText: 'Title',
+              ),
+              const SizedBox(
+                height: 18,
+              ),
+               CustomTextField(
+                 onSaved: (value){
+                   subTitle = value;
+                 },
+                hintText: 'Content',
+                maxLines: 5,
+              ),
+              const SizedBox(
+                height: 62,
+              ),
+               CustomButton(
+                onTap: (){
+                  if(formKey.currentState!.validate()){
+                    formKey.currentState!.save();
+                  }
+                  else{
+                    autoValidateMode = AutovalidateMode.always;
+                    setState(() {
+
+                    });
+                  }
+                },
+              ),
+              const SizedBox(
+                height: 18,
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -40,26 +71,29 @@ class AddNoteBottomSheet extends StatelessWidget {
 }
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({Key? key}) : super(key: key);
-
+  const CustomButton({Key? key, this.onTap}) : super(key: key);
+  final void Function()? onTap;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: 50,
-      decoration: const BoxDecoration(
-        color: kPrimaryColor,
-        borderRadius: BorderRadius.all(
-          Radius.circular(18),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 50,
+        decoration: const BoxDecoration(
+          color: kPrimaryColor,
+          borderRadius: BorderRadius.all(
+            Radius.circular(18),
+          ),
         ),
-      ),
-      child: const Center(
-        child: Text(
-          'Add',
-          style: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-            fontSize: 16,
+        child: const Center(
+          child: Text(
+            'Add',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
           ),
         ),
       ),
